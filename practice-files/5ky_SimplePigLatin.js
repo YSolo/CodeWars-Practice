48f815369db581c48de8af0929a3d3984988952e
Eugene <eugene.solohub@gmail.com>
2020-04-16 05:51:37 +0300

    Need to learn RegEx

---
+++ practice-files/5ky_SimplePigLatin.js
@@ -0,0 +1,33 @@
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+// My first solution
+
+function pigIt(str){
+  const words = str.split(" ");
+  
+  const convertToPigLatin = word => {
+    if (word === '!' || word === '?') return word;
+  
+    const letters = word.split('');
+    const firstLetter = letters.shift();
+    
+    return `${letters.join('')}${firstLetter}ay`;
+  }
+  
+  return words.map(convertToPigLatin).join(' ');
+}
+
+
+// Best practice
+
+function pigIt(str){
+  return str.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
+}
+
+// Takeaway
+
+// Learn about regular expressions.
