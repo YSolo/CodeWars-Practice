877354989cc7a8b4574ee340af9696c0c3977eb6
Eugene <eugene.solohub@gmail.com>
2020-04-25 06:10:48 +0300

    Add solution to Snail Sort (unfinished)

--- practice-files/4ky_StailSort.js
+++ practice-files/4ky_StailSort.js
@@ -25,3 +25,28 @@ NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array 
 
 // My solution
 
+snail = function(array) {
+  const snailArray = [...array];
+  
+  const crawlingPath = [];
+
+  crawl(snailArray);
+  
+  return crawlingPath;
+
+  function crawl(array) {
+    // default case
+    if(array[0].length === 0) return;
+    
+    // if there is first row remaining
+    if(array[0].length > 1) {
+      crawlingPath.concat(array[0].splice(0, array[0].length - 1));
+      
+    // if thereis only one way - down
+    } else if (array[0].length === 1) {
+      for (let row = 0; row < array.length - 1; row++) {
+        
+      }
+    }
+  }
+}
